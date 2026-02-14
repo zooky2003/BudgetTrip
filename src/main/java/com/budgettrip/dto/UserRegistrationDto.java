3abb9b2a7c5f77ba388ff885b53d145c5b1c1989
Hasitha635 <hasithadeshappriya635@gmail.com>
2026-02-14 21:09:23 +0530

    Create UserRegistrationDto.java

---
+++ src/main/java/com/budgettrip/dto/UserRegistrationDto.java
@@ -0,0 +1,22 @@
+package com.budgettrip.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegistrationDto {
+
+    @NotEmpty(message = "Username is required")
+    private String username;
+
+    @NotEmpty(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotEmpty(message = "Password is required")
+    private String password;
+    private String preference;
+}
